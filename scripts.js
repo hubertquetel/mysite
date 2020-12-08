2a0dd7b4544f53a3069d9f3fd61eb5548067c6e7
Hubert QUETEL <hubertquetel@me.com>
2020-12-08 11:07:33 +0100

    Update scripts.js
    Commit 2

--- scripts.js
+++ scripts.js
@@ -1,27 +1,21 @@
+// Finsweet Cookies
+
 <script src="https://cdnjs.cloudflare.com/ajax/libs/js-cookie/2.2.0/js.cookie.min.js"></script>
 
 <script>
-    $(document).ready(function() {
-  function playGif(gifclass){
-    const gif = $(gifclass); 
-    gif[0].src = gif[0].src.replace(/\?.*$/,"")+"?x="+Math.random(); 
-  }
   
-  const seenGif = Cookies.get('seenGif');
-  if(!seenGif){
+    const seenBanner = Cookies.get('seenBanner');
+  if(!seenBanner){
     $('.hack6-loading-wrapper').css('display', 'flex');
-    playGif('.hack6-gif');
-    Cookies.set('seenGif', true, { expires: 1 });
+    Cookies.set('seenBanner', true, { expires: 1 });
 
     setTimeout(()=>{
       $('.hack6-loading-wrapper').fadeOut(500);
     }, 4500);
   }
 
-  // this code is only for our hack instance! You don't need it in your code
-  // when .clear-cookie is clicked 
   $('.clear-cookie').click(()=>{
-    Cookies.remove('seenGif');
+    Cookies.remove('seenBanner');
   });
 });
 </script>
